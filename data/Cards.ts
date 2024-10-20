c0dfcd516afd4f532a1f1df6cd4c53330848fb85
Giovanni <usr3855@salesianos-lacuesta.net>
2024-10-20 15:39:23 +0100

    modifications Card

--- data/Cards.ts
+++ data/Cards.ts
@@ -1,9 +1,9 @@
 import { CardProps } from "../components/Card";
 
 export const cards: CardProps[] = [
-    {
-        title: 'Descripción sobre mí!',
-        body: 'Soy profe y me gusta mi trabajo aunque a veces me de por enrevesar prácticas para mis queridos alumnos',
-        imgSource: require('../assets/img/SofyanAmrabat.jpg')
-    }
-]
+  {
+    title: "Descripción sobre mí!",
+    body: "Soy Giovanni, un chico pensador que le gustar conmigo mismo y me gusta la programacion aunque me cueste y lo intento entender porque me gusta.",
+    imgSource: require("../assets/img/saber.png"),
+  },
+];
